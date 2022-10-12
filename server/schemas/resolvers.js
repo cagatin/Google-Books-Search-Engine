bb6367858a227166a010eb51ced1cac66b5fbdfc
Felix Cagatin <cagatingilbert@gmail.com>
2022-10-12 08:47:15 -0700

    Implemented the Apollo Server and applied server to express server as middleware

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,2 @@
+// Define the query and mutation functionality to work w/ the mongoose models
+// Hint: Use the functionality in the user-controller.js as a guide.
--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Import the apollo server 
+const { ApolloServer } = require('apollo-server-express');
+
+// Import the typeDefs and resovlers
+const { typeDefs, resolvers } = require('./schemas')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create an Instance of the Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,9 +28,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Create a new instance of an Apollo Server w/ the GQL schema
+// Implement apollo server and apply it to the express server as middleware
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQ: at https://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
 
-
-// TODO: Implement apollo server and apply it to the express server as middleware
