f7952ff4ec4a972d3cfe3cdb387fc74fc09ec68c
Felix Cagatin <cagatingilbert@gmail.com>
2022-10-12 08:55:22 -0700

    Updated auth middlewear to work w/ GraphQL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,17 +1,16 @@
 const jwt = require('jsonwebtoken');
 
-// TODO: update the auth middleware function to work w/ the GQL API
-
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // Split token into an array
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
@@ -23,6 +22,7 @@ module.exports = {
 
     // verify token and get user data out of it
     try {
+      // if the token is verified, add the decoded user's data to the request so it can be accessed in resolver
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
@@ -30,8 +30,8 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // Return the request objext so it can be passed to the resolver as 'context'
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
