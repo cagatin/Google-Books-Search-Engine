45a1ed4435d37959df36ca6d2e3703dd9066d9b6
Gil <Cagagtingilbert@gmail.com>
2022-10-14 11:35:12 -0700

    Added LOGIN_USER mutation in client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -8,4 +8,49 @@ ADD_USER will execute the addUser mutation.
 SAVE_BOOK will execute the saveBook mutation.
 
 REMOVE_BOOK will execute the removeBook mutation.
- */ 
+ */
+
+
+import { gql } from '@apollo/client'
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                }
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                }
+            }
+        }
+    }
+`
