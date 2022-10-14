bf50c496d2ba1188e1f3d87a8c998c0d424cd2de
Gil <Cagagtingilbert@gmail.com>
2022-10-14 10:24:06 -0700

    added the login and addUser mutations to resolver file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,75 +1,49 @@
 // Define the query and mutation functionality to work w/ the mongoose models
 // Hint: Use the functionality in the user-controller.js as a guide.
+import { User } from '../models';
+import { signToken } from '../utils/auth';
+const { AuthenticationError } = require('apollo-server-express');
 
-/*
-module.exports = {
-  // get a single user by either their id or their username
-  async getSingleUser({ user = null, params }, res) {
-    const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-    });
+const resolvers = {
+  Query: {
+    me: async () => {
 
-    if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
-    }
-
-    res.json(foundUser);
-  },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-  async createUser({ body }, res) {
-    const user = await User.create(body);
-
-    if (!user) {
-      return res.status(400).json({ message: 'Something is wrong!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
-  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
-  async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
-    if (!user) {
-      return res.status(400).json({ message: "Can't find this user" });
-    }
-
-    const correctPw = await user.isCorrectPassword(body.password);
-
-    if (!correctPw) {
-      return res.status(400).json({ message: 'Wrong password!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-  // user comes from `req.user` created in the auth middleware function
-  async saveBook({ user, body }, res) {
-    console.log(user);
-    try {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { savedBooks: body } },
-        { new: true, runValidators: true }
-      );
-      return res.json(updatedUser);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json(err);
     }
   },
-  // remove a book from `savedBooks`
-  async deleteBook({ user, params }, res) {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
-      { new: true }
-    );
-    if (!updatedUser) {
-      return res.status(404).json({ message: "Couldn't find user with this id!" });
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      // Find a user based on the Email
+      const userData = await User.findOne({ email });
+
+      // If no user matches the email, throw an Authentication Error
+      if (!userData) {
+        throw new AuthenticationError("No User found that matches the email");
+      }
+
+      // Use the isCorrectPassword method of the User model to verify the password
+      const passwordCheck = userData.isCorrectPassword(password);
+
+      // Check if the password used is correct
+      if (!passwordCheck) {
+        throw new AuthenticationError(`Incorrect Password for ${email}!`);
+      }
+
+      // If the password matches the email, sign the token
+      const token = signToken(userData);
+
+      // return the User and the token
+      return { token, profile }
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      // Create the user
+      const newUser = await User.create({ username, email, password });
+
+      // Then sign the token
+      const token = signToken(newUser);
+
+      return { token, newUser };
     }
-    return res.json(updatedUser);
-  },
-};
-
-
-*/
+  }
+}
