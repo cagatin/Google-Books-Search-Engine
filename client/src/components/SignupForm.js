83096e95ac4fdc8966e21fa0256c23262d3c9e41
Felix Cagatin <cagatingilbert@gmail.com>
2022-10-15 14:33:02 -0700

    replaced addUser() functionality with the ADD_USER mutation funcitonality

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +11,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  // bind the ADD_USER mutation to our addUser function
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +31,22 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // Add the user using our addUser mutation function
+      const { data } = await addUser({
+        variables: {
+          username: userFormData.username,
+          email: userFormData.email,
+          password: userFormData.password,
+        }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      // if an error exists, throw an error
+      if (!data.addUser.user) {
+        throw new Error('Cannot add user!');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      console.log(data.addUser.user);   // log the new user data
+      Auth.login(data.addUser.token);   // log in the new user
     } catch (err) {
       console.error(err);
       setShowAlert(true);
